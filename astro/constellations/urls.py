e8461721cadaf7e6719eeb2c6fde8ecd5bb68be8
yan4iks <janamihaleva12@gmail.com>
2025-10-20 20:04:56 +0300

    pages

--- astro/constellations/urls.py
+++ astro/constellations/urls.py
@@ -4,7 +4,12 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('<int:page>/', index, name='home_page'),
     path('movies/<slug:slug>/', movie_detail, name= 'movie_detail'),
     path('genres/<int:pk>/', movies_by_genre, name = 'movies_by_genre'),
-    path('directors/<int:pk>/', movies_by_director, name = 'movies_by_director')
+    path('genres/<int:pk>/page<int:page>/', movies_by_genre, name='movies_by_genre_page'),
+    path('directors/<int:pk>/', movies_by_director, name = 'movies_by_director'),
+    path('directors/<int:pk>/page<int:page>/', movies_by_director, name='movies_by_director_page'),
+    path('actors/<int:pk>/', actor_detail, name = 'actor_detail'),
+    path('actors/<int:pk>/page<int:page>/', actor_detail, name= 'actor_detail_page')
 ]
--- astro/constellations/views.py
+++ astro/constellations/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from django.http import HttpResponse
-from .models import Movie, Genre, Director
+from .models import Movie, Genre, Director, Actor
 
 def index(request):
     movies = Movie.objects.filter(is_published = True)
 
+    paginator = Paginator(movies, 4)
+    page_number = request.GET.get('page')
+
+    try:
+        page_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+
     context = {
-        'movie_list': movies,
+        # 'movie_list': movies,
+        'page_obj': page_obj,
         'title': "Кинокаталог - Главная"
     }
 
@@ -27,18 +40,45 @@ def movies_by_genre(request, pk):
     genre = get_object_or_404(Genre, pk=pk)
     movies = genre.movies.filter(is_published=True)
 
+    paginator = Paginator(movies, 4)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'movies_list': movies,
+        # 'movies_list': movies,
+        'page_obj': page_obj,
         'title': f'Фильмы в жанре: {genre.name}'
     }
     return render(request, 'constellations/movie_list.html', context=context)
 
 def movies_by_director(request, pk):
-    genre = get_object_or_404(Director, pk=pk)
-    movies = genre.movies.filter(is_published=True)
+    director = get_object_or_404(Director, pk=pk) 
+    movies = director.movies.filter(is_published=True)  
+
+    paginator = Paginator(movies, 4)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'movies_list': movies,
-        'title': f'Фильмы режиссёра: {director}'
+        'page_obj': page_obj,
+        'title': f'Фильмы режиссёра: {director}'  
     }
     return render(request, 'constellations/movie_list.html', context=context)
+
+
+def actor_detail(request, pk):
+    actor = get_object_or_404(Actor, pk=pk)
+    movies = actor.movies.filter(is_published = True)
+
+    paginator = Paginator(movies, 4)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'actor': actor,
+        # 'movies_list': movies,
+        'page_obj': page_obj,
+        'title': f'Информация об актёре: {actor}'
+    }
+
+    return render(request, 'constellations/actor_detail.html', context=context)
